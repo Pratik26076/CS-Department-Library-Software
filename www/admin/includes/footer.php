c6fd0f1d5afe12b51455caeb63bcd476198125e9
Pratik26076 <102870641+Pratik26076@users.noreply.github.com>
2024-07-30 22:48:32 +0530

    Add files via upload

---
+++ www/admin/includes/footer.php
@@ -0,0 +1,12 @@
+<section class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                &copy;
+                <?php echo date('Y'); ?> VPKBIET COMPUTER DEPARTMENT LIBRARY |<a href="https://phpgurukul.com/"
+                    target="_blank"> Designed by : PHPGURUKUL</a>
+            </div>
+
+        </div>
+    </div>
+</section>
